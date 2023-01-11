e65faf4defa10dcddbd10bfc46eeacc1ea7657f1
Marc Bertran <mbertransuarez@gmail.com>
2023-01-11 16:47:54 +0100

    Iteration 3 done

--- routes/drones.js
+++ routes/drones.js
@@ -16,12 +16,18 @@ router.get('/drones', async (req, res, next) => {
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form');
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  try {
+    await Drones.create({name, propellers, maxSpeed});
+    res.redirect('/drones');
+  } catch (error) {
+    res.redirect('/drones/create');
+  }
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
